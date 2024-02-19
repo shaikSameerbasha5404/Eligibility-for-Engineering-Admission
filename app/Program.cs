97750cdac3fc649a79a5d01ded93629edabe7709
shaikSameerbasha5404 <sameerbasha8125@gmail.com>
2024-02-19 11:33:24 +0530

    hi

---
+++ app/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EngineeringAdmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maths, physics, chemistry;
+            Console.WriteLine("Welcome to Engineering Admission Eligibility Checker");
+
+            Console.Write("Enter Math marks: ");
+            maths = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Physics marks: ");
+            physics = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Chemistry marks: ");
+            chemistry = Convert.ToInt32(Console.ReadLine());
+
+            int totalMarks = maths + physics + chemistry;
+
+            if (maths >= 65 && physics >= 55 && chemistry >= 50)
+            {
+                if (totalMarks >= 180)
+                {
+                    Console.WriteLine("Congratulations! You are eligible for admission.");
+
+                }
+                
+            }
+            else
+                {
+                    Console.WriteLine("Sorry, you are not eligible for admission.");
+
+                }
+        }
+    }
+}
