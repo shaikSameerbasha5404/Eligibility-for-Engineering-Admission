098d62aee3c02637fcbb611d2cab2c2e75e3e1d4
shaikSameerbasha5404 <sameerbasha8125@gmail.com>
2024-02-19 11:51:19 +0530

    hi

--- Program.cs
+++ Program.cs
@@ -1,9 +1,39 @@
 ﻿using System;
-    class Eligibility
+
+namespace EngineeringAdmission
+{
+    class Porogram
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Hiiiiiii");
-        }
-    }
+            int maths, physics, chemistry;
+            Console.WriteLine("Welcome to Engineering Admission Eligibility Checker");
 
+            Console.Write("Enter Math marks: ");
+            maths = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Physics marks: ");
+            physics = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Chemistry marks: ");
+            chemistry = Convert.ToInt32(Console.ReadLine());
+
+            int totalMarks = maths + physics + chemistry;
+
+            if (maths >= 65 && physics >= 55 && chemistry >= 50)
+            {
+                if (totalMarks >= 180)
+                {
+                    Console.WriteLine("Congratulations! You are eligible for admission.");
+
+                }
+                
+            }
+            else
+                {
+                    Console.WriteLine("Sorry, you are not eligible for admission.");
+
+                }
+        }
+    }
+}
